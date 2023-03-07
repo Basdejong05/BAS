c6402e26cdde05c3d198524032991358a89d8093
mau <9014816@student.zadkine.nl>
2023-03-07 13:12:08 +0100

    update

--- connectie.php
+++ connectie.php
@@ -1,1 +1,22 @@
 <?php
+
+    $user = localhost;
+    $pass = root;
+
+$dbh = new PDO('mysql:host=localhost;dbname=bas', $user, $pass);
+
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=bas', $user, $pass);
+    foreach($dbh->query('SELECT * from FOO') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+
+?>
+
+
