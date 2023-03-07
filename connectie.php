348b0d79854f26a625b06ee96c7c363b54ec4f78
mau <9014816@student.zadkine.nl>
2023-03-07 13:23:44 +0100

    update

--- connectie.php
+++ connectie.php
@@ -1,22 +1,21 @@
 <?php
 
-    $user = localhost;
-    $pass = root;
+    $user = 'localhost';
+    $pass = 'root';
 
-$dbh = new PDO('mysql:host=localhost;dbname=bas', $user, $pass);
-
-try {
     $dbh = new PDO('mysql:host=localhost;dbname=bas', $user, $pass);
-    foreach($dbh->query('SELECT * from FOO') as $row) {
-        print_r($row);
+
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=bas', $user, $pass);
+        foreach($dbh->query('SELECT * from FOO') as $row) {
+            print_r($row);
+        }
+        $dbh = null;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
     }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
 
 
-?>
 
 
